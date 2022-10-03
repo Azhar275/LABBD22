392cd509efcbc9329442a8d1c4c06358a601b591
RifkyAqid10 <muhrifkyaqid@gmail.com>
2022-10-03 12:14:52 +0800

    Tugas 2

---
+++ H071211077/Tugas Praktikum 2/Nomor1-5.sql
@@ -0,0 +1,11 @@
+-- use classicmodels
+-- nomor 1
+-- SELECT * FROM offices WHERE city = 'San Francisco';
+-- nomor 2
+-- SELECT * FROM orderdetails WHERE quantityOrdered >70 ORDER BY orderLineNumber ASC;
+-- -- nomor 3
+-- SELECT DISTINCT productLine FROM products;
+-- -- nomor 4
+-- SELECT customerNumber AS 'Nomor', customerName AS 'Nama' FROM customers WHERE customerNumber >=100 AND customerNumber <=150;
+-- -- nomor 5
+-- SELECT * FROM customers WHERE country != 'USA' AND creditLimit =  '0' ORDER BY customerName ASC LIMIT 10,10
