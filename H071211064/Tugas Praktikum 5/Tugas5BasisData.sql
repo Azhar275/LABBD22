fb3868b6195bbadc88c246c39a4e682a960d4e1a
AgungArdhiyanda <114483578+ardhiyandaa76@users.noreply.github.com>
2022-10-21 17:41:47 +0800

    Add missing code

--- H071211064/Tugas Praktikum 5/Tugas5BasisData.sql
+++ H071211064/Tugas Praktikum 5/Tugas5BasisData.sql
@@ -1,4 +1,4 @@
-use classicmodels;
+-- use classicmodels;
 
 -- 1
 /*
@@ -20,7 +20,7 @@ WHERE orders.comments LIKE '%DHL%';
 */
 
 -- 3 
-
+/*
 SELECT customers.customerName, orders.`status`, products.productName, orders.orderDate
 FROM customers
 INNER JOIN orders
@@ -30,5 +30,20 @@ ON orders.orderNumber = orderdetails.orderNumber
 INNER JOIN products
 ON orderdetails.productCode = products.productCode
 WHERE products.productName LIKE '%Ferrari%' ORDER BY orders.orderDate DESC;
+*/
 
 -- 4
+-- SELECT * FROM orders ORDER BY orderNumber DESC;
+-- orders, orderdetails
+-- SELECT * FROM products
+-- WHERE productName LIKE '%ford V8%';
+/*
+ INSERT INTO orders(orderNumber, orderDate, requiredDate, status, customerNumber)
+ VALUES(10426 , current_date, adddate(current_date, interval 1 year), 'In Process', 465)
+
+*/
+ -- INSERT INTO orderdetails
+-- VALUES(10426, 'S18_2957', 50, 62.46, 1)
+-- SELECT * FROM customers 
+-- INNER JOIN orders ON customers.customerNumber = orders.customerNumber INNER JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber 
+-- WHERE customers.customerName LIKE '%anton%'*/
